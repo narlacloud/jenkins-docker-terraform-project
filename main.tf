ef0a6c19a1471ad569331a459322968fe9a51b70
narlacloud <55445733+narlacloud@users.noreply.github.com>
2021-10-13 17:32:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_instance" "web1" {
   subnet_id = "${aws_subnet.subnet1-public.id}"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
   tags = {
-    Name = "Server-1"
+    Name = "DOCKER-2"
     Terraform   = "true"
     Environment = "dev"
   }
